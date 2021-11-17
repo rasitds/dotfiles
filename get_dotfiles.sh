c13963840c08b526a4f86c934925fbc5746203fc
00101111 <12167194+00101111@users.noreply.github.com>
2021-11-17 18:48:27 +0300

    initial files

---
+++ get_dotfiles.sh
@@ -0,0 +1,7 @@
+cp ~/.bashrc files/bashrc
+cp ~/.Xresources files/Xresources
+cp ~/.vimrc files/vimrc
+cp ~/.config/i3/config files/i3_config
+cp ~/.config/i3status/config files/i3status_config
+cp /etc/httpd/conf/httpd.conf files/httpd.conf
+cp /etc/php/php.ini files/php.ini

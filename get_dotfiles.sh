57c4aed6bd120392a64b3d19431b62db8dd81f5a
00101111 <12167194+00101111@users.noreply.github.com>
2021-11-18 20:56:56 +0300

    add file

--- get_dotfiles.sh
+++ get_dotfiles.sh
@@ -5,3 +5,4 @@ cp ~/.config/i3/config dotfiles/i3_config
 cp ~/.config/i3status/config dotfiles/i3status_config
 cp /etc/httpd/conf/httpd.conf dotfiles/httpd.conf
 cp /etc/php/php.ini dotfiles/php.ini
+cp /etc/ly/config.ini dotfiles/ly_config.ini

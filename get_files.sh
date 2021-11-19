91c715277bc444fb1b9da3135b2fdb750904d907
00101111 <12167194+00101111@users.noreply.github.com>
2021-11-19 21:48:16 +0300

    update

--- get_files.sh
+++ get_files.sh
@@ -3,6 +3,6 @@ cp ~/.Xresources dotfiles/Xresources
 cp ~/.vimrc dotfiles/vimrc
 cp ~/.config/i3/config dotfiles/i3_config
 cp ~/.config/i3status/config dotfiles/i3status_config
-cp /etc/httpd/conf/httpd.conf dotfiles/httpd.conf
-cp /etc/php/php.ini dotfiles/php.ini
-cp /etc/ly/config.ini dotfiles/ly_config.ini
+cp /etc/httpd/conf/httpd.conf cfgs/httpd.conf
+cp /etc/php/php.ini cfgs/php.ini
+cp /etc/ly/config.ini cfgs/ly_config.ini

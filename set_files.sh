d227b46c7f0681ea90f0ac7a42c4459432a83a18
00101111 <12167194+00101111@users.noreply.github.com>
2021-11-23 20:06:33 +0300

    update

---
+++ set_files.sh
@@ -0,0 +1,9 @@
+cp dotfiles/bashrc ~/.bashrc
+cp dotfiles/Xresources ~/.Xresources
+xrdb .Xresources
+cp dotfiles/vimrc ~/.vimrc
+cp dotfiles/i3_config ~/.config/i3/config
+cp dotfiles/i3status_config ~/.config/i3status/config
+sudo cp cfgs/httpd.conf /etc/httpd/conf/httpd.conf 
+sudo cp cfgs/php.ini /etc/php/php.ini
+sudo cp cfgs/ly_config.ini /etc/ly/config.ini

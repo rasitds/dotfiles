3dac1515fe3f2fcf9e463769172774cc9ced44fc
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-05-30 11:11:23 +0000

    fix: wrong paths

--- install.sh
+++ install.sh
@@ -55,10 +55,10 @@ cp home/Xresources ~/.Xresources
 cp home/xinitrc ~/.xinitrc
 xrdb -merge ~/.Xresources
 
-cp home/vimrc ~/.vimrc
 cp -R home/config/nvim ~/.config/nvim
 
-cp home/i3/config ~/.config/i3/config
-cp home/i3status_config ~/.config/i3status/config
+cp home/config/i3/config ~/.config/i3/config
+mkdir -p /home/$USER/.config/i3status
+cp home/config/i3status/config ~/.config/i3status/config
 
 cp home/gitconfig ~/.gitconfig
